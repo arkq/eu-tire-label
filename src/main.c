577bfc4afcabeee3ef78d4cc486aed57b4c3b44d
Arkadiusz Bokowy <arkadiusz.bokowy@gmail.com>
2016-04-23 10:56:40 +0200

    Fix typo in the license header

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
  *
  * This file is a part of an EU-tire-label.
  *
- * This projected is licensed under the terms of the MIT license.
+ * This project is licensed under the terms of the MIT license.
  *
  */
 
